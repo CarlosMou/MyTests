c0c90acf5d2523bfe3b96869fdb0cf5cce5314b0
Carlos Moura Jr <cmouraj@gmail.com>
2021-04-17 01:18:27 -0400

    Quem e voce no filme de terror?

--- teste1.py
+++ teste1.py
@@ -1,3 +1,19 @@
-print ('heloo')
+#Quem voce seria num filme de terror?
+
+print ('You are READY?')
+resposta = input()
+if resposta == 'no':
+	print ('All right we begin!')
+	print ('What is your weight?')
+	weight = input
+if weight >= str(90):
+	print ('You are the on dies first')
+else:	
+	print ('End Game')
+ 
+
+#comando para inserir peso altura, e idade
+#se tiver x ele pode ser o que morre primeiro, o que se esconde, o que mata o vilao ou o que transa
+
 
 
