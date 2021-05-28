6e614b6791623faa41c3023258db3c85befac33f
Carlos Moura Jr <cmouraj@gmail.com>
2021-05-27 22:16:22 -0400

    For hour this is final test

--- teste1.py
+++ teste1.py
@@ -1,8 +1,9 @@
-#Quem voce seria num filme de terror?
+#Who are you in horror movie?
+#This aplication is a game simulation who you would be in a movie, based on a date such as weight and age.
 
 print ('You are READY?')
-resposta = input()
-if resposta == 'no':
+answer = input()
+if answer == 'no':
 	print ('All right we begin!')
 	print ('What is your weight?')
 	weight = input()	
@@ -12,10 +13,6 @@ elif weight <= (str(18)):
 	print ('You are the on who hides')
 else:
 	print ('You live in film')
-
-	
-#comando para inserir peso altura, e idade
-#se tiver x ele pode ser o que morre primeiro, o se vive.
 
 
 
